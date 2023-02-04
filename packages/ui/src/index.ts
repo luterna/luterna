826b1fd9d87febec08748d70252ddf558db1fc8b
Bohuslav Semenov <semenov0310@gmail.com>
2023-02-04 13:22:27 +0100

    feat: change useLuterna to createLuterna

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,12 +1,2 @@
-import type { Plugin } from 'vue';
-import * as components from './components';
-
-export const useLuterna: Plugin = {
-  install: (app) => {
-    Object.keys(components).forEach((name) => {
-      app.component(name, components[name]);
-    });
-  },
-};
-
 export * as components from './components';
+export { createLuterna } from './plugin';
