bccaa84b047315ab30b880760aa499eb971543c3
Bohuslav Semenov <semenov0310@gmail.com>
2023-02-05 15:15:37 +0100

    fix: fix missing style import into plugin

--- packages/ui/src/plugin/create-luterna.ts
+++ packages/ui/src/plugin/create-luterna.ts
@@ -1,6 +1,8 @@
 import type { Plugin } from 'vue';
 import * as components from '@/components';
 
+import '@/assets/global.scss';
+
 export const createLuterna = (): Plugin => ({
   install: (app) => {
     Object.keys(components).forEach((name) => {
