3ee5ca1d4b9e471191f537757bed27deba10d3f5
Bohuslav Semenov <semenov0310@gmail.com>
2023-12-02 11:42:28 +0100

    chore(histoire): add LuternaBorderRadiuses story

---
+++ packages/ui/src/styles/stories/LuternaBorderRadiuses.story.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import { useLuternaConfig } from '@/plugin';
+</script>
+
+<template>
+  <Story title="Radiuses" group="styles">
+    <div class="flex">
+      <div class="block" v-for="(value, borderRadius) in useLuternaConfig().theme.borderRadius" :key="borderRadius">
+        <div
+          class="block__item"
+          :style="{
+            borderRadius: value,
+          }"
+        ></div>
+        {{ borderRadius }} ({{ value }})
+      </div>
+    </div>
+  </Story>
+</template>
+
+<style lang="scss" scoped>
+.flex {
+  display: flex;
+  gap: 1rem;
+}
+.block {
+  text-align: center;
+  &__item {
+    height: 100px;
+    aspect-ratio: 1 / 1;
+    background-color: rgb(var(--lu-primary-5));
+  }
+}
+</style>
