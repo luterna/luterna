ef14fe70de57d04c1a944f26b4160a68cf412a5a
Bohuslav Semenov <semenov0310@gmail.com>
2023-12-02 11:43:25 +0100

    chore(LuButton): add change of size and radius

--- packages/ui/src/components/lu-button/LuButton.story.vue
+++ packages/ui/src/components/lu-button/LuButton.story.vue
@@ -3,11 +3,13 @@ import { reactive } from 'vue';
 import LuButton from './LuButton.vue';
 import { logEvent } from 'histoire/client';
 import { LuButtonVariant } from './types';
-import { AllowedColors } from '@/plugin/types';
+import { AllowedColors, LuternaSizes } from '@/plugin/types';
 
 const state = reactive<InstanceType<typeof LuButton>['$props'] & { content: string }>({
   variant: 'filled',
   content: 'I am a button',
+  radius: 'sm',
+  size: 'sm',
   color: 'primary',
   disabled: false,
 });
@@ -17,9 +19,11 @@ const state = reactive<InstanceType<typeof LuButton>['$props'] & { content: stri
   <Story title="LuButton" group="buttons" autoPropsDisabled>
     <LuButton v-bind="state" @click="logEvent('click', $event)"> {{ state.content }}</LuButton>
     <template #controls>
-      <HstSelect v-model="state.variant" title="variant" :options="LuButtonVariant" />
-      <HstSelect v-model="state.color" title="color" :options="AllowedColors" />
-      <HstCheckbox v-model="state.disabled" title="disabled" />
+      <HstButtonGroup v-model="state.variant" title="Label" :options="LuButtonVariant" />
+      <HstButtonGroup v-model="state.radius" title="Radius" :options="LuternaSizes" />
+      <HstButtonGroup v-model="state.size" title="Size" :options="LuternaSizes" />
+      <HstSelect v-model="state.color" title="Color" :options="AllowedColors" />
+      <HstCheckbox v-model="state.disabled" title="Disabled" />
     </template>
   </Story>
 </template>
--- packages/ui/src/components/lu-button/LuButton.vue
+++ packages/ui/src/components/lu-button/LuButton.vue
@@ -1,22 +1,26 @@
 <script lang="ts" setup>
-import type { AllowedColors } from '@/plugin/types';
+import type { LuternaSizes, AllowedColors } from '@/plugin/types';
 import type { LuButtonVariant } from './types';
 
 export type Props = {
   variant?: LuButtonVariant;
   disabled?: boolean;
   color?: AllowedColors;
+  radius?: LuternaSizes;
+  size?: LuternaSizes;
 };
 
 withDefaults(defineProps<Props>(), {
   variant: 'filled',
   disabled: false,
   color: 'primary',
+  radius: 'sm',
+  size: 'sm',
 });
 </script>
 
 <template>
-  <button :class="['lu-button', variant, color, { disabled }]">
+  <button :class="['lu-button', variant, color, size, `radius-${radius}`, { disabled }]">
     <span class="lu-button__text"><slot /></span>
   </button>
 </template>
@@ -25,11 +29,8 @@ withDefaults(defineProps<Props>(), {
 .lu-button {
   $parent: &;
 
-  padding: 0 1rem;
   font-weight: 600;
   outline: none;
-  height: 2.25rem;
-  border-radius: 0.25rem;
   transition: all 0.25s ease;
   cursor: pointer;
 
@@ -66,6 +67,56 @@ withDefaults(defineProps<Props>(), {
   &.dark {
     --lu-color: var(--lu-dark-6);
     --lu-hover-color: var(--lu-dark-8);
+  }
+
+  &.radius-xs {
+    border-radius: var(--lu-borderRadius-xs);
+  }
+
+  &.radius-sm {
+    border-radius: var(--lu-borderRadius-sm);
+  }
+
+  &.radius-md {
+    border-radius: var(--lu-borderRadius-md);
+  }
+
+  &.radius-lg {
+    border-radius: var(--lu-borderRadius-lg);
+  }
+
+  &.radius-xl {
+    border-radius: var(--lu-borderRadius-xl);
+  }
+
+  &.xs {
+    height: 1.875rem;
+    padding: 0 0.875rem;
+    font-size: var(--lu-fontSize-xs);
+  }
+
+  &.sm {
+    height: 2.25rem;
+    padding: 0 1.125rem;
+    font-size: var(--lu-fontSize-sm);
+  }
+
+  &.md {
+    height: 2.625rem;
+    padding: 0 1.375rem;
+    font-size: var(--lu-fontSize-md);
+  }
+
+  &.lg {
+    height: 3.125rem;
+    padding: 0 1.625rem;
+    font-size: var(--lu-fontSize-lg);
+  }
+
+  &.xl {
+    height: 3.75rem;
+    padding: 0 2rem;
+    font-size: var(--lu-fontSize-xl);
   }
 
   &.filled {
